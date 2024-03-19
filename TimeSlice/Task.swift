75a791088445f15c992deda27973d50fbd3530d0
Katelyn Zheng <katelynz@andrew.cmu.edu>
2024-03-19 16:51:07 -0400

    Add initializer and complete status toggler method to Task struct q

--- TimeSlice/Task.swift
+++ TimeSlice/Task.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+struct Task:Identifiable {
+    let id: UUID
+    var title: String
+    var isComplete: Bool
+    
+    init(id: UUID, title: String, isComplete: Bool) {
+        self.id = id
+        self.title = title
+        self.isComplete = isComplete
+    }
+    
+    /*
+    method to toggle complete status for a Task
+    if isComplete == true and isComplete.toggle() -> isComplete == false
+    if isComplete == false and isComplete.toggle() -> isComplete == true
+    */
+    mutating func toggleIsComplete(){
+        isComplete.toggle()
+    }
+    
+    
+    
+}
