e199125d4753583b567d35ccee2f857a53ddc7e2
Katelyn Zheng <katelynz@andrew.cmu.edu>
2024-03-19 20:11:24 -0400

    Add methods to ensure proper functioning of timer, including switching modes between work and break, and adding/pausing/resetting. Add functionality to both View and ViewModel for alerts.

--- TimeSlice/PomodoroTimerView.swift
+++ TimeSlice/PomodoroTimerView.swift
@@ -10,8 +10,15 @@ import SwiftUI
 
 struct PomodoroTimerView: View {
     var task: Task
+    @ObservedObject var viewModel: PomodoroTimerViewModel
 
     var body: some View {
         Text("Timer for \(task.title)")
+        
+            // Alerts user when onBreak is toggled, depending on state of onBreak
+            .alert(isPresented: $viewModel.showAlert) {
+                Alert(title: Text(viewModel.alertTitle))
+            }
     }
+    
 }
